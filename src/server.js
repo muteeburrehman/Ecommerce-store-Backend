616224f06524268e3e3079e0d2e86a9801b06f14
muteeburrehman <muteeb285@gmail.com>
2023-08-15 20:07:14 +0500

    connectTo

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios=require('axios');
+const axios = require('axios');
 
 const products = [{
     id: '123',
@@ -100,9 +100,76 @@ export let cartItems = [
 const app = express();
 app.use(bodyParser.json());
 
+// Connect to the SQLite database
+const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) return console.error(err.message);
+
+    console.log('Connected to the database');
+
+    // Create the products table if it doesn't exist
+    // Create the products table if it doesn't exist
+    db.run(`
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            price DOUBLE,
+            description TEXT,
+            imageUrl TEXT,
+            averageRating TEXT
+        )
+    `, (createErr) => {
+        if (createErr) {
+            console.error('Error creating products table:', createErr.message);
+        } else {
+            console.log('Products table created successfully');
+        }
+    });
+
+
+    // Create the users table if it doesn't exist
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            PhoneNo TEXT,
+            cartItems TEXT DEFAULT ''
+        )
+    `, (createErr) => {
+        if (createErr) {
+            console.error('Error creating users table:', createErr.message);
+        } else {
+            console.log('Users table created successfully');
+        }
+    });
+
+    db.run(`
+       CREATE TABLE IF NOT EXISTS carts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER, 
+        cart_item_id INTEGER,
+        product_id INTEGER,
+        quantity INTEGER,
+        price DECIMAL(10, 2),
+        total_price DECIMAL(10, 2),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (product_id) REFERENCES products(id)
+    )
+
+    `, (createErr) => {
+        if (createErr) {
+            console.error('Error creating users table:', createErr.message);
+        } else {
+            console.log('Carts table created successfully');
+        }
+    });
+});
+
+// ... Other parts of your code ...
+
 async function queryDatabase(query, params) {
     return new Promise((resolve, reject) => {
-        const db = new sqlite3.Database('vue-db.sqlite');
+        const db = new sqlite3.Database('./mock.db');
         db.all(query, params, (err, rows) => {
             if (err) {
                 reject(err);
@@ -119,29 +186,24 @@ app.get('/api/products', async (req, res) => {
         const rows = await queryDatabase('SELECT * FROM products', []);
         res.status(200).json(rows);
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({error: 'Database error'});
     }
 });
 
+// API route to get a user's cart items
 app.get('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params;
 
     try {
-        const user = await queryDatabase('SELECT * FROM users WHERE id = ?', [userId]);
-        if (!user[0]) {
-            res.status(404).json('Could not find user!');
-            return;
-        }
-        const cartItemIds = user[0].cartItems.split(',');
-        const rows = await queryDatabase('SELECT * FROM products WHERE id IN (?)', [cartItemIds]);
-        res.status(200).json(rows);
+        const cartItems = await queryDatabase('SELECT * FROM carts WHERE user_id = ?', [userId]);
+        res.status(200).json(cartItems);
     } catch (err) {
         res.status(500).json({ error: 'Database error' });
     }
 });
 
 app.get('/api/products/:productId', async (req, res) => {
-    const { productId } = req.params;
+    const {productId} = req.params;
 
     try {
         const product = await queryDatabase('SELECT * FROM products WHERE id = ?', [productId]);
@@ -151,37 +213,211 @@ app.get('/api/products/:productId', async (req, res) => {
         }
         res.status(200).json(product[0]);
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({error: 'Database error'});
     }
 });
 
+/*
 app.post('/api/users/:userId/cart', async (req, res) => {
-    const { userId } = req.params;
-    const { productId } = req.body;
+    const {userId} = req.params;
+    const {productId} = req.body;
 
     try {
-        await queryDatabase('UPDATE users SET cartItems = cartItems || ? || "," WHERE id = ?', [productId, userId]);
         const product = await queryDatabase('SELECT * FROM products WHERE id = ?', [productId]);
         if (!product[0]) {
             res.status(404).json('Could not find product!');
             return;
         }
+        await queryDatabase('UPDATE users SET cartItems = cartItems || ? || "," WHERE id = ?', [productId, userId]);
         res.status(200).json(product[0]);
     } catch (err) {
-        res.status(500).json({ error: 'Database error' });
+        res.status(500).json({error: 'Database error'});
     }
 });
+*/
 
+/*
 app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
-    const { userId, productId } = req.params;
+    const {userId, productId} = req.params;
 
     try {
         await queryDatabase('UPDATE users SET cartItems = REPLACE(cartItems, ? || ",", "") WHERE id = ?', [productId, userId]);
         const rows = await queryDatabase('SELECT * FROM products WHERE id IN (SELECT cartItems FROM users WHERE id = ?)', [userId]);
         res.status(200).json(rows);
     } catch (err) {
+        res.status(500).json({error: 'Database error'});
+    }
+});
+*/
+
+//insert,update,modify
+
+app.post('/api/products', (req, res) => {
+    const {id, name, price, description, imageUrl, averageRating} = req.body;
+
+    const insertQuery = `
+    INSERT INTO products (name, price, description, imageUrl, averageRating)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+    db.run(insertQuery, [name, price, description, imageUrl, averageRating], (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({error: 'Failed to insert product'});
+        } else {
+            res.status(201).json({message: 'Product inserted successfully'});
+        }
+    });
+});
+
+// API route to add a product to a user's cart
+app.post('/api/users/:userId/cart', async (req, res) => {
+    const { userId } = req.params;
+    const { product_id, quantity } = req.body;
+
+    try {
+        // Fetch product details from the products table
+        const product = await queryDatabase('SELECT * FROM products WHERE id = ?', [product_id]);
+        if (!product[0]) {
+            res.status(404).json('Could not find product!');
+            return;
+        }
+
+        // Fetch the cart item details
+        const cartItem = await queryDatabase('SELECT * FROM carts WHERE product_id = ? AND user_id = ?', [product_id, userId]);
+        if (cartItem[0]) {
+            // Calculate total price based on updated quantity
+            const _qty = parseInt(cartItem[0].quantity) + quantity;
+            const price = parseFloat(product[0].price);
+            const total_price = price * _qty;
+
+            // Update the cart item with new quantity and total price
+            const updateQuery = `
+            UPDATE carts
+            SET quantity = ?, price = ?, total_price = ?
+            WHERE product_id = ?
+        `;
+            await queryDatabase(updateQuery, [_qty, price, total_price, product_id]);
+
+            res.status(200).json({ message: 'Cart item updated successfully' });
+            return;
+        }
+
+        // Calculate total price based on product price and quantity
+        const price = parseFloat(product[0].price);
+        const total_price = price * quantity;
+
+        // Insert the item into the cart
+        const insertQuery = `
+            INSERT INTO carts (user_id, product_id, quantity, price, total_price)
+            VALUES (?, ?, ?, ?, ?)
+        `;
+        await queryDatabase(insertQuery, [userId, product_id, quantity, price, total_price]);
+
+        res.status(200).json({ message: 'Product added to cart successfully' });
+    } catch (err) {
+        res.status(500).json({ error: 'Database error:' + err });
+    }
+});
+// API route to update a product based on productId
+app.put('/api/products/:productId', (req, res) => {
+    const {productId} = req.params;
+    const {name, price, description, imageUrl, averageRating} = req.body;
+
+    const updateQuery = `
+    UPDATE products
+    SET name = ?, price = ?, description = ?, imageUrl = ?, averageRating = ?
+    WHERE id = ?
+  `;
+
+    db.run(updateQuery, [name, price, description, imageUrl, averageRating, productId], (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({error: 'Failed to update product'});
+        } else {
+            res.status(200).json({message: 'Product updated successfully'});
+        }
+    });
+});
+
+// API route to update the quantity of a product in a user's cart
+app.put('/api/users/:userId/cart/:cartItemId', async (req, res) => {
+    const { userId, cartItemId } = req.params;
+    const { quantity } = req.body;
+
+    try {
+        // Fetch the cart item details
+        const cartItem = await queryDatabase('SELECT * FROM carts WHERE id = ? AND user_id = ?', [cartItemId, userId]);
+        if (!cartItem[0]) {
+            res.status(404).json('Could not find cart item!');
+            return;
+        }
+
+        // Fetch product details from the products table
+        const product = await queryDatabase('SELECT * FROM products WHERE id = ?', [cartItem[0].product_id]);
+        if (!product[0]) {
+            res.status(404).json('Could not find product!');
+            return;
+        }
+
+        // Calculate total price based on updated quantity
+        const price = parseFloat(product[0].price);
+        const total_price = price * quantity;
+
+        // Update the cart item with new quantity and total price
+        const updateQuery = `
+            UPDATE carts
+            SET quantity = ?, price = ?, total_price = ?
+            WHERE id = ?
+        `;
+        await queryDatabase(updateQuery, [quantity, price, total_price, cartItemId]);
+
+        res.status(200).json({ message: 'Cart item updated successfully' });
+    } catch (err) {
         res.status(500).json({ error: 'Database error' });
     }
+});
+
+app.delete('/api/users/:userId/cart/:cartItemId', async (req, res) => {
+    const { userId, cartItemId } = req.params;
+
+    try {
+        // Fetch the cart item details
+        const cartItem = await queryDatabase('SELECT * FROM carts WHERE id = ? AND user_id = ?', [cartItemId, userId]);
+        if (!cartItem[0]) {
+            res.status(404).json({ message: 'Could not find cart item!' });
+            return;
+        }
+
+        // Delete the item from the cart
+        const deleteQuery = `
+            DELETE FROM carts
+            WHERE id = ? AND user_id = ?
+        `;
+        await queryDatabase(deleteQuery, [cartItemId, userId]);
+
+        res.status(200).json({ message: 'Product removed from cart successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+// API route to delete a product based on productId
+app.delete('/api/products/:productId', (req, res) => {
+    const {productId} = req.params;
+
+    const deleteQuery = `
+    DELETE FROM products
+    WHERE id = ?
+  `;
+
+    db.run(deleteQuery, [productId], (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({error: 'Failed to delete product'});
+        } else {
+            res.status(200).json({message: 'Product deleted successfully'});
+        }
+    });
 });
 
 app.listen(8000, () => {
